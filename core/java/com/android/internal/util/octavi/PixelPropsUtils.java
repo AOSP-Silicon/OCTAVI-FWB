Evolution-X-AOSP/frameworks_base

PPU: Another day, another cat and mouse chase

core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -231,18 +231,12 @@ public class PixelPropsUtils {
                 if (was) return true;
 
                 dlog("Spoofing build for GMS");
-                // Alter model name and fingerprint to Asus Zenfone 4 to avoid hardware attestation enforcement
-                setPropValue("BRAND", "Asus");
-                setPropValue("PRODUCT", "WW_Phone");
-                setPropValue("MODEL", "ASUS_X00HD");
-                setPropValue("MANUFACTURER", "Asus");
-                setPropValue("DEVICE", "ASUS_X00HD_4");
-                setPropValue("FINGERPRINT", "asus/WW_Phone/ASUS_X00HD_4:7.1.1/NMF26F/14.2016.1801.372-20180119:user/release-keys");
-                setPropValue("ID", "NMF26F");
-                setPropValue("TYPE", "user");
-                setPropValue("TAGS", "release-keys");
-                setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
-                setVersionFieldString("SECURITY_PATCH", "2018-01-01");
+                // Alter build parameters to Nexus 6P for avoiding hardware attestation enforcement
+                setPropValue("DEVICE", "bullhead");
+                setPropValue("FINGERPRINT", "google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys");
+                setPropValue("MODEL", "Nexus 5X");
+                setPropValue("PRODUCT", "bullhead");
+                setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N);
                 return true;
             }
         }

AOSP-Silicon/OCTAVI-FWB

Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

core/java/com/android/internal/util/octavi/PixelPropsUtils.java
@@ -378,12 +378,12 @@ public class PixelPropsUtils {
     }
 
     private static void spoofBuildGms() {
-        // Alter model name and fingerprint to avoid hardware attestation enforcement
-        setBuildField("FINGERPRINT", "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys");
-        setBuildField("PRODUCT", "marlin");
-        setBuildField("DEVICE", "marlin");
-        setBuildField("MODEL", "Pixel XL");
-        setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
+        // Alter model name and fingerprint to Nexus 6P for avoiding hardware attestation enforcement
+        setBuildField("FINGERPRINT", "google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys");
+        setBuildField("PRODUCT", "bullhead");
+        setBuildField("DEVICE", "bullhead");
+        setBuildField("MODEL", "Nexus 5X");
+        setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N);
     }
 
 